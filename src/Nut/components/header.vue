440b797541f0681bf8a292cf8ad087856e08434a
railken <railken@protonmail.ch>
2017-07-24 18:28:21 +0200

    Improved header

--- src/Nut/components/header.vue
+++ src/Nut/components/header.vue
@@ -1,6 +1,6 @@
 <template>
    
-    <header class='fluid'>
+    <header class='fluid fluid-vcenter'>
 
         <div>
             Track your timey wimey stuff
@@ -13,19 +13,31 @@
             <!--<i class="fa fa-refresh rotate-1" aria-hidden="true"></i>
             <span class='text-sm'>SYNCING</span>-->
 
-            <i class="fa fa-refresh" aria-hidden="true"></i>
-            <span class='text-sm'>IN SYNC</span>
+            <!--<i class="fa fa-refresh" aria-hidden="true"></i>
+            <span class='text-sm'>IN SYNC</span>-->
         </div>
         <div>
             &nbsp;&nbsp;
         </div>
-        <div class='header-container-user'>
-            {{ user.username }}
+        <div>
+            <img v-bind:src='user.avatar' width='25' height='25' id='user-avatar'>
         </div>
         <div>
             &nbsp;&nbsp;
         </div>
-        <span v-on:click="logout()"><i class='fa fa-sign-out'></i></span>
+        <div class='fluid fluid-stretch dropdown'>
+            <div class='fluid fluid-vcenter fill' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="user-action">
+         
+                <i class='fa fa-caret-down'></i>
+            </div>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="nav-team-actions">
+
+                <a href="javascript:;" class="dropdown-item">Signed in as {{ user.username }}</a>
+                <div class="dropdown-divider"></div>
+                <a href="javascript:;" class="dropdown-item">Settings</a>
+                <a href="javascript:;" class="dropdown-item" v-on:click="logout()">Sign out</a>
+            </div>
+        </div>
 
     </header>
         
@@ -53,17 +65,31 @@
 </script>
 
 <style>
-    
+
     header {
         background: #505061;
         color: white;
-        padding: 15px;
+        height: 51px;
         border-bottom: solid 1px #3d3d47;
         box-shadow: 0 1px 2px rgba(0,0,0,0.15);   
         z-index: 3;
+        padding: 0 15px;
         position: relative;
         position: fixed;
         width: 100%;
         top: 0;
+    }
+
+    #user-action {
+        opacity: 0.7;
+    }
+
+    #user-action:hover {
+        opacity: 1.0;
+    }
+    
+    #user-avatar {
+        border-radius: 100%;
+        border:1px solid #3a3a43;
     }
 </style>
