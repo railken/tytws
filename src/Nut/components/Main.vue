519e37618a4aa3e221a030d9a5d5c3e02f6a6a97
railken <railken@protonmail.ch>
2017-07-24 20:33:37 +0200

    improved layout dashboard

--- src/Nut/components/Main.vue
+++ src/Nut/components/Main.vue
@@ -6,8 +6,34 @@
             <div class='container-teams'>
                 <component-team-list></component-team-list>
             </div>
-            <div class='container-content'>
-                Dashboard
+            <div class='container-content fill'>
+                <div class='paper content-spacing'>
+                    <h1>Dashboard</h1>
+                </div>
+
+                <div class='paper content-spacing'>
+                    <select class='form-control'>
+                        <option>Today</option>
+                        <option>Yesterday</option>
+                        <option>Current Week</option>
+                        <option>Previous Week</option>
+                        <option>Current Month</option>
+                        <option>Previous Month</option> 
+                    </select>
+                </div>
+
+                <div class='paper content-spacing'>
+                    Sum of total hours
+                </div>
+
+                <div class='paper content-spacing'>
+                    chart1:  hours x days 
+                </div>
+
+                <div class='paper content-spacing'>
+                    chart2: how many hours x hour of day 
+                </div>
+
             </div>
         </div>
     </main>
