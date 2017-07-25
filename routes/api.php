9c766e578d082719c10aeef2ea00d7b8853275ae
railken <railken@protonmail.ch>
2017-07-25 19:43:22 +0200

    moved api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function() {
+    Route::any('/oauth/{name}/access_token', ['uses' => '\Api\Http\Controllers\Auth\SignInController@accessToken']);
+    Route::any('/oauth/{name}/exchange_token', ['uses' => '\Api\Http\Controllers\Auth\SignInController@exchangeToken']);
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::any('/user', ['uses' => '\Api\Http\Controllers\User\UserController@index']);
+
+        Route::group(['prefix' => '/user/teams'], function () {
+            Route::get('/', ['uses' => '\Api\Http\Controllers\User\TeamsController@index']);
+            Route::post('/', ['uses' => '\Api\Http\Controllers\User\TeamsController@create']);
+            Route::get('/{id}', ['uses' => '\Api\Http\Controllers\User\TeamsController@show']);
+            Route::put('/{id}', ['uses' => '\Api\Http\Controllers\User\TeamsController@update']);
+            Route::delete('/{ids}', ['uses' => '\Api\Http\Controllers\User\TeamsController@delete']);
+        });
+
+        
+        Route::group(['prefix' => '/user/activities'], function () {
+            Route::get('/', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@index']);
+            Route::post('/', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@create']);
+            Route::get('/{id}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@show']);
+            Route::put('/{id}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@update']);
+            Route::delete('/{ids}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@delete']);
+        });
+
+
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-
-Route::group(['prefix' => 'api/v1'], function() {
-	Route::any('/oauth/{name}/access_token', ['uses' => '\Api\Http\Controllers\Auth\SignInController@accessToken']);
-	Route::any('/oauth/{name}/exchange_token', ['uses' => '\Api\Http\Controllers\Auth\SignInController@exchangeToken']);
-
-	Route::group(['middleware' => 'auth:api'], function() {
-		Route::any('/user', ['uses' => '\Api\Http\Controllers\User\UserController@index']);
-
-		Route::group(['prefix' => '/user/teams'], function () {
-            Route::get('/', ['uses' => '\Api\Http\Controllers\User\TeamsController@index']);
-            Route::post('/', ['uses' => '\Api\Http\Controllers\User\TeamsController@create']);
-            Route::get('/{id}', ['uses' => '\Api\Http\Controllers\User\TeamsController@show']);
-            Route::put('/{id}', ['uses' => '\Api\Http\Controllers\User\TeamsController@update']);
-            Route::delete('/{ids}', ['uses' => '\Api\Http\Controllers\User\TeamsController@delete']);
-        });
-
-        
-        Route::group(['prefix' => '/user/activities'], function () {
-            Route::get('/', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@index']);
-            Route::post('/', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@create']);
-            Route::get('/{id}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@show']);
-            Route::put('/{id}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@update']);
-            Route::delete('/{ids}', ['uses' => '\Api\Http\Controllers\User\ActivitiesController@delete']);
-        });
-
-
-	});
-});
-
 Route::any('/{all}', function () {
     return view('app');
-})->where(['all' => '.*']);
+})->where(['all' => '^((?!api).)*']);
